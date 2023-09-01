cdc54693f9bb4a96763e8d25a517fdc16675b5bf
choi11223344 <choi890220@gmail.com>
2023-09-01 10:09:31 +0900

    first commit

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+include_once('./_common.php');
+
+// 페이지 제목(웹접근성 관련)
+
+include_once(G5_PATH.'/head.php');
+$g5['title'] = '회사소개';
+add_stylesheet('<link rel="stylesheet" href="'.G5_CSS_URL.'/sub.css">', 0);
+?>
+
+about
+
+<?php
+include_once(G5_PATH.'/tail.php');
